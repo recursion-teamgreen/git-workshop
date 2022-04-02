239c3a5dcec813aed067d66c15d8d53b0c90bdf3
Takahir-O <87518531+Takahir-O@users.noreply.github.com>
2022-04-02 16:43:24 +0900

    途中作業だがエラー発生のため一旦コミット

--- scripts.js
+++ scripts.js
@@ -20,3 +20,27 @@ function scroll_event(){
     scroll_to_top_btn.style.opacity ='0'
   }
 };
+
+
+const section = document.querySelector('.cherry-blossom-container');
+
+
+const createPetal=()=>{
+  const petalEl = document.createElement('span');
+  petalEl.className = 'petal';
+  const minSize = 10;
+  const maxSize = 15;
+  const size = Math.random()*(maxSize+1-minSize)+minSize;
+  petalEl.style.width = `${size}px`;
+  petalEl.style.height = `${size}px`;
+  petalEl.style.left = Math.random()*innerWidth+'px';
+  section.appendChild(petalEl);
+
+  setTimeout(()=>{
+    petalEl.remove();
+  },10000);
+}
+
+var changeImage = document.getElementById('changeImage');
+
+changeImage.addEventListener('click',setInterval(createPetal,300),false);
